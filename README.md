7820f2d3c423e7e121654d1a21b65e27974b3a31
Ryan Asher <rasher@tribune.com>
2015-07-01 09:06:49 -0500

    Implement user profile templates and views:
    Create views and templates for user profiles and frontend following logic.
    
    Update the README to reflect some TODOs.

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Django Twitter Clone
 
 Built using the tutorial from [tuts+](http://code.tutsplus.com/tutorials/building-ribbit-in-django--net-29957).
+
+## TODOS:
+
+The tutorial didn't cover writing tests for the code, so at some point down the road, it might be fun to come back to this project and write some tests to cover our work.
+
+Clean up long-running lines to better conform to PEP 8 standards.
--- ribbit/urls.py
+++ ribbit/urls.py
@@ -18,9 +18,12 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^$', 'ribbit_app.views.index', name='home'),
+    url(r'^follow$', 'ribbit_app.views.follow', name='follow'),
     url(r'^login$', 'ribbit_app.views.login_view', name='login'),
     url(r'^logout$', 'ribbit_app.views.logout_view', name='logout'),
     url(r'^ribbits$', 'ribbit_app.views.public', name='ribbits'),
-    url(r'^submit$', 'ribbit_app.views.submit', name='submit'),
     url(r'^signup$', 'ribbit_app.views.signup', name='signup'),
+    url(r'^submit$', 'ribbit_app.views.submit', name='submit'),
+    url(r'^users/$', 'ribbit_app.views.users', name='users'),
+    url(r'^users/(?P<username>\w{0,30})/$', 'ribbit_app.views.users'),
 ]
